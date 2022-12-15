4041131bd10458e0239ef6f97896884cb296f3b4
Pratham <Chaurasia.1@iitj.ac.in>
2022-12-15 20:18:37 +0530

    Created About, details need to be added

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div class = "container mx-3 my-5 text-center">
+        <p>
+            This is Defi application developed to imitate the behaviour of a online shop powered by blockchain on Polygon Testnet.
+        </p>
+    </div>
+  )
+}
