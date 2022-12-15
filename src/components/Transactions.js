d60b1e6cf6624ee012891edf5a33b2598f2f7412
Pratham <Chaurasia.1@iitj.ac.in>
2022-12-15 20:24:48 +0530

    Transactions page created to get the history of the user

---
+++ src/components/Transactions.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useState } from 'react'
+
+import { useEffect } from 'react'
+
+
+export const Transactions = (props) => {
+
+
+
+
+  return (
+    <>
+    <div class = "container">Check later</div>
+    </>
+
+  )
+}
