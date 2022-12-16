f68776c8876c6d8c6caa4006f33d70190cab5620
PrathamGit-dev <96198632+PrathamGit-dev@users.noreply.github.com>
2022-12-16 18:49:47 +0530

    update about

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,11 @@ export const About = () => {
     <div class = "container mx-3 my-5 text-center">
         <p>
             This is Defi application developed to imitate the behaviour of a online shop powered by blockchain on Polygon Testnet.
+            The project is under deployment with features being continuously updated. You can give your feedback on the following github repository.
+    <a href="https://github.com/PrathamGit-dev/BlockchainShop" target="_blank">Click here for GITHUB repository</a>.
         </p>
+    </p>
+    
     </div>
   )
 }
